85bc9a2105a13aae23cc1be835b7767c6e769e15
Yuxin Wu <ppwwyyxxc@gmail.com>
2017-04-01 11:38:49 -0700

    update script

--- preprocess.sh
+++ preprocess.sh
@@ -1,9 +1,13 @@
-#!/bin/bash -e
+#!/bin/zsh -e
 # File: preprocess.sh
 # Author: Yuxin Wu <ppwwyyxx@gmail.com>
+
+[[ -z "$1" ]] && exit 1
 
 which parallel > /dev/null 2>&1 && {
 	find "$1" -type f -name '*.html' | parallel ./transform.py '{}'
 } || {
 	find "$1" -type f -name '*.html' -exec ./transform.py {} \;
 }
+
+find "$1" -type f -name "*.mp4" -delete
