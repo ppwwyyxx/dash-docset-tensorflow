c8c84b17bdfa6a45122244f84edbb41797a439bc
Yuxin Wu <ppwwyyxxc@gmail.com>
2017-05-02 02:43:10 -0700

    update

--- preprocess.sh
+++ preprocess.sh
@@ -5,7 +5,7 @@
 [[ -z "$1" ]] && exit 1
 
 which parallel > /dev/null 2>&1 && {
-	find "$1" -type f -name '*.html' | parallel ./transform.py '{}'
+	find "$1" -type f -name '*.html' | parallel --eta ./transform.py '{}'
 } || {
 	find "$1" -type f -name '*.html' -exec ./transform.py {} \;
 }
