1a7f2109f7d860d5b475b2b4c1b94eed2b763bcd
Yuxin Wu <ppwwyyxxc@gmail.com>
2017-12-21 14:25:11 -0800

    udpates

--- preprocess.sh
+++ preprocess.sh
@@ -4,15 +4,16 @@
 
 [[ -z "$1" ]] && exit 1
 
+set +e
+rm -rf "$1"/versions/master/api_docs/python/tf/contrib/keras*
+rm -rf "$1"/versions/master/api_docs/java
+set -e
+
+
 which parallel > /dev/null 2>&1 && {
 	find "$1" -type f -name '*.html' | parallel --eta ./transform.py '{}'
 } || {
 	find "$1" -type f -name '*.html' -exec ./transform.py {} \;
 }
-
-set +e
-rm -rf "$1"/versions/master/api_docs/python/tf/contrib/keras*
-rm -rf "$1"/versions/master/api_docs/java
-set -e
 
 find "$1" -type f -name "*.mp4" -delete
